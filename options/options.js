e20d7fcf98e16a471cc2f21a09f4ac3a31207c9b
Crowdbotics <engineering@crowdbotics.com>
2023-07-26 10:18:19 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "billowing-dream-de-104375",
+  url: "https://billowing-dream-de-104375.botics.co",
+  api: "https://billowing-dream-de-104375.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
